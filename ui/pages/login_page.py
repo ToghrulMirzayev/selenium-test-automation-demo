afa0e52b4a11e4c12324be625a247ee9c2b6c478
ToghrulMirzayev <togrul.mirzoev@gmail.com>
2023-03-22 05:40:27 +0100

    code slightly updated

--- ui/pages/login_page.py
+++ ui/pages/login_page.py
@@ -4,30 +4,29 @@ from ui.elements.base_elements import InputElement, ActionElement
 class LoginPage:
     """Page Object representing the login page"""
 
-    username = InputElement((By.ID, "user-name"))
-    password = InputElement((By.ID, "password"))
-    login_button = ActionElement((By.ID, "login-button"))
-    title_text = InputElement((By.CSS_SELECTOR, ".title"))
-    error_text = InputElement((By.CSS_SELECTOR, ".error-message-container"))
+    USERNAME = InputElement((By.ID, "user-name"))
+    PASSWORD = InputElement((By.ID, "password"))
+    LOGIN_BTN = ActionElement((By.ID, "login-button"))
+    TITLE_TEXT = InputElement((By.CSS_SELECTOR, ".title"))
+    ERROR_TEXT = InputElement((By.CSS_SELECTOR, ".error-message-container"))
 
     def __init__(self, driver):
         self.driver = driver
 
     def login(self, username, password):
         """Logs in using the provided username and password"""
-        self.username.clear()
-        self.username.send_keys(username)
-        self.password.clear()
-        self.password.send_keys(password)
-        self.login_button.click()
+        self.USERNAME.clear()
+        self.USERNAME.send_keys(username)
+        self.PASSWORD.clear()
+        self.PASSWORD.send_keys(password)
+        self.LOGIN_BTN.click()
 
     def verify_main_title(self, expected):
         expected_result = expected
-        actual_result = self.title_text.text
+        actual_result = self.TITLE_TEXT.text
         assert actual_result == expected_result, f"Expected {expected_result}, but got {actual_result}"
 
     def verify_error_message(self, expected):
         expected_result = expected
-        actual_result = self.error_text.text
+        actual_result = self.ERROR_TEXT.text
         assert actual_result == expected_result, f"Expected {expected_result}, but got {actual_result}"
-
